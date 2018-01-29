6ea79264496d1d1998194859d63c37987a315f82
артем рыбка < >
2018-01-29 20:11:26 +0300

    Add project files.

---
+++ webstore/Controllers/EmployeesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using webstore.Models;
+
+namespace webstore.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEnumerable<EmployerViewModel> _employees = new List<EmployerViewModel>
+        {
+            new EmployerViewModel
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SurName = "Иванов",
+                Patronymic = "Иванович",
+                Age = 22,
+                Details = "Детали первого"
+            },
+            new EmployerViewModel
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                SurName = "Петров",
+                Patronymic = "Иванович",
+                Age = 35,
+                Details = "Детали второго"
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+
+            var employee = _employees.FirstOrDefault(t => t.Id == id);
+
+            return View(employee);
+        }
+
+    }
+}
---
+++ webstore/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace webstore
+{
+    public class Startup
+    {
+
+        // доступ к файлу конфигурации
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Добавляем расширение для использования статических файлов, т.к. appsettings.json -
+            //это статический файл
+
+            app.UseStaticFiles();
+
+            var hello = Configuration["CustomHelloWord"];
+
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync(hello);
+            //});
+
+            //Добавляем обработку запросов в mvc-формате
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
